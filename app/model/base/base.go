a14152c2dc69d9e70797f7176f6b5330d1d1fff4
yandi <yandi.bastian@gmail.com>
2022-02-16 11:29:08 +0700

    timezone model

--- app/model/base/base.go
+++ app/model/base/base.go
@@ -3,6 +3,8 @@ package base
 import (
 	"time"
 
+	"go-klikdokter/pkg/util"
+
 	gonanoid "github.com/matoous/go-nanoid/v2"
 	"gorm.io/gorm"
 )
@@ -17,22 +19,22 @@ type BaseIDModel struct {
 	UID string `gorm:"uniqueIndex" json:"uid"`
 
 	IsDeleted bool   `json:"is_deleted"`
-	CreatedBy string `json:"-"`
-	CreatedAt string `json:"-"`
-	UpdatedBy string `json:"-"`
-	UpdatedAt string `json:"-"`
+	CreatedBy string `json:"-" gorm:"type:varchar"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedBy string `json:"-" gorm:"type:varchar"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 func (base *BaseIDModel) BeforeCreate(tx *gorm.DB) error {
 	uid, _ := gonanoid.New()
 	tx.Statement.SetColumn("UID", uid)
 	tx.Statement.SetColumn("IsDeleted", false)
-	tx.Statement.SetColumn("CreatedAt", time.Now().UTC().Format("2006-01-02 15:04:05"))
-	tx.Statement.SetColumn("UpdatedAt", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	tx.Statement.SetColumn("CreatedAt", time.Now().In(util.Loc).Format("2006-01-02 15:04:05"))
+	tx.Statement.SetColumn("UpdatedAt", time.Now().In(util.Loc).Format("2006-01-02 15:04:05"))
 	return nil
 }
 
 func (base *BaseIDModel) BeforeUpdate(tx *gorm.DB) error {
-	tx.Statement.SetColumn("UpdatedAt", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	tx.Statement.SetColumn("UpdatedAt", time.Now().In(util.Loc).Format("2006-01-02 15:04:05"))
 	return nil
 }
--- app/model/entity/product.go
+++ app/model/entity/product.go
@@ -9,15 +9,15 @@ type Product struct {
 	base.BaseIDModel
 	// Name of the product
 	// in: string
-	Name string `json:"name" faker:"first_name"`
+	Name string `json:"name" gorm:"not null;type:varchar" faker:"first_name"`
 
 	// Sku of the product
 	// in: string
-	Sku string `json:"sku" faker:"time_period"`
+	Sku string `json:"sku" gorm:"type:varchar" faker:"time_period"`
 
 	// Uom of the product
 	// in: string
-	Uom string `json:"uom" faker:"uuid_digit"`
+	Uom string `json:"uom" gorm:"type:varchar" faker:"uuid_digit"`
 
 	// Weight of the product
 	// in: int32
